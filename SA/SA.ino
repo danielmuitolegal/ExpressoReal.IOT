165763978d3dabf7db69bc8297fcc2311a350ab9
sjmsaramoraes <sara_moraes@estudante.sesisenai.org.br>
2025-10-21 08:37:01 -0300

    sara

--- SA/SA.ino
+++ SA/SA.ino
@@ -14,6 +14,7 @@ const String PASS = "8120gv08";
 // Endereço e porta do broker MQTT público
 const String brokerURL = "test.mosquitto.org";
 const int brokerPort = 1883;
+const String topic = "gatinhos";
 
 // Usuário e senha do broker (aqui estão vazios porque esse broker é público)
 const String brokerUser = "";
@@ -44,13 +45,35 @@ void setup() {
   // Tenta conectar ao broker MQTT
   while (mqtt.connect(clientID.c_str()) == 0) {
     Serial.print(".");               // Mostra pontos enquanto tenta conectar
-    delay(200);
+    delay(2000);
   }
-
+  mqtt.subscribe(topic.c_str());
+  mqtt.setCallback(callback);
   Serial.println("\nConectado ao broker MQTT!");
 }
 
 void loop() {
   // Aqui vai o código que será executado continuamente
   // (por exemplo, publicar ou receber mensagens MQTT)
+  String mensagem = "";
+  if(Serial.available() > 0){
+    mensagem = Serial.readStringUntil('\n');
+    mensagem = "sara: " + mensagem;
+    mqtt.publish("bob",mensagem.c_str());
+    mqtt.publish("macaco",mensagem.c_str());
+    mqtt.publish("felpz",mensagem.c_str());
 }
+mqtt.loop();
+}
+
+void callback(char* topic, byte* payload, unsigned long length){
+  String MensagemRecebida = "";
+  for (int i = 0; i < length; i++){
+    //Pega cada letra de payload e junta na mensagem
+    MensagemRecebida += (char) payload[i];
+
+  }
+  Serial.println(MensagemRecebida);
+
+}
+
